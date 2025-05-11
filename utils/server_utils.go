6d99475ae1dc91a12d14efb373d8e6d5452d09bf
andrewkizito <andrewkizito54@gmail.com>
2025-05-11 12:09:46 +0300

    feat: Refactor AddCacheEntry function to include error handling and base64 encoding for cache entries

--- utils/server_utils.go
+++ utils/server_utils.go
@@ -1,9 +1,35 @@
 package utils
 
-import "fmt"
+import (
+	"encoding/base64"
+	"errors"
+)
 
-func AddCacheEntry() {
-	fmt.Println("Saving to cache")
+type RawCacheEntry struct {
+	ContentType string
+	Raw         []byte
+}
+
+func AddCacheEntry(key string, data RawCacheEntry) (string, error) {
+	if key == "" || len(data.Raw) == 0 {
+		return "", errors.New("key and data can not be empty")
+	}
+
+	hashedKey := GenerateHash(key)
+	encodedData := base64.StdEncoding.EncodeToString(data.Raw)
+
+	exists := HasKey(hashedKey)
+	if exists {
+		return "", errors.New("key is already taken, use update action or delete entry")
+	}
+
+	newCacheEntry := CacheEntry{
+		ContentType: data.ContentType,
+		Data:        encodedData,
+	}
+
+	SetData(hashedKey, newCacheEntry)
+	return key, nil
 }
 
 func ReadCachEntry() {
